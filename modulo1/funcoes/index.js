6384a5f7b450ed5b1e5f93616ac3a3871d1e95bc
Eric Felipe Silva e Silva <ericfelipess07@gmail.com>
2022-02-17 16:33:41 -0300

    Subindo funções

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,22 @@
+//1-
+//a. o valor de 10 e 0 de 50 
+//b. udefined, já que não solicitou que mostrasse no consola
+
+//2-
+//a. armazena o que o usuário escreveu e transforma em minusculo.
+//b. I-true, II-true, III-true.
+//--------------------------------------------------------------------------------------------------
+ 
+function frase(nome, idade, cidade, prof){
+ let frase = "Eu sou Caio, tenho 23 anos, moro em São Paulo e sou estudante."    
+ let nome = "Eric"
+ let  idade = "19anos"
+ let  cidade = "Campo Alegre"
+ let prof = "Estudante"
+
+ return 
+} 
+
+
+    
+
