ab41fe794199dd89404d5201a9c94f68a8be2255
Eric Felipe Silva e Silva <ericfelipess07@gmail.com>
2022-02-17 22:21:41 -0300

    Subindo o resto do exercicio de funcoes

--- modulo1/funcoes/index.js
+++ modulo1/funcoes/index.js
@@ -7,16 +7,84 @@
 //b. I-true, II-true, III-true.
 //--------------------------------------------------------------------------------------------------
  
-function frase(nome, idade, cidade, prof){
- let frase = "Eu sou Caio, tenho 23 anos, moro em São Paulo e sou estudante."    
- let nome = "Eric"
- let  idade = "19anos"
- let  cidade = "Campo Alegre"
- let prof = "Estudante"
+//1-
+/*function exercicio1() {
+    let nome = "Eric"
+    let  idade = "19"
+    let  cidade = "Campo Alegre"
+    let prof = "Estudante"
+    let frase = `Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${prof}.` 
 
- return 
+    return frase
+}
+s
+exercicio1()*/
+
+/*function exercicio2a() {
+    let numero1 = 10
+    let numero2 = 20
+    Number
+    let soma = numero1 + numero2
+    console.log(soma)
+
 } 
+exercicio2a()
+
+function exercicio2b(){
+    let n1 = 15
+    let n2 = 10
+    Number
+    let somar = n1 > n2 
+    console.log(somar)
+}
+
+exercicio2b()
+
+function exercicio2c (){
+let numPa = 22 
+let divisor = 2 
+restoDaDivsao = numPa%divisor
+
+console.log(restoDaDivsao,0!==true)
+}
+exercicio2c()*/
+
+//3-
+/*function exercicio3(){
+let number1 = 30
+let number2 = 3
+soma = number1 + number2
+subtracao = number1 - number2
+multiplicacao = number1 * number2
+divisao = number1 / number2
+
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
+}
+
+exercicio3()*/
+
+//Desafio 1
+/*const somar = (a, b) =>{
+    const soma = a + b
+    return soma
+}
+
+const soma1 = somar(44, 20)
+console.log(soma1)*/
+
+/*Desafio 2
+function hip (a, b){
+    const soma = ((a*a)+(b*b))**0.5
+    return soma
+
+}
+
+const soma1 = hip(3, 4)
+console.log(soma1)*/
 
 
-    
+
 
