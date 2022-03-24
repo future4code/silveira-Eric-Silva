dfda94ceacd62eefcf3bd0ffebf30a793b877659
Eric Felipe Silva e Silva <ericfelipess07@gmail.com>
2022-03-24 15:56:30 -0300

    lista-insta4

--- modulo2/estados-insta4/insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/insta4/src/App.js
@@ -10,27 +10,53 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
-  render() {
+  state= {
+    Post:[
+        {
+        nomeUsuario:"paulinha",
+        fotoUsuario:"https://picsum.photos/50/50",
+        fotoPost:"https://picsum.photos/200/150"
+        },
+        {
+        nomeUsuario:"irineu",
+        fotoUsuario:"https://picsum.photos/50/60",
+        fotoPost: "https://picsum.photos/200/160"
+        },
+        {
+        nomeUsuario:"hommer",
+        fotoUsuario:"https://picsum.photos/50/70",
+        fotoPost:"https://picsum.photos/200/170"
+        }
+      ],
+      valorInputNome:"",
+      valorInputFotoPerfil:"",
+      valorInputFotoPost:""
+    };
+      adicionaPessoa = () => {
+        const novaPessoa = {
+          nomeUsuario: this.state.valorInputNome,
+          fotoUsuario: this.state.valorInputFotoPerfil,
+          fotoPost: this.state.valorInputFotoPost
+        }
+      }
+
+render() {
+  const arrayDeComponentes = this.state.Post.map((item) => {
+
+    return (
+      <Post
+        {...item}
+      />
+    )
+  })
+
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-        nomeUsuario={'Irineu'}
-        fotoUsuario={'https://picsum.photos/50/60'}
-        fotoPost={'https://picsum.photos/200/160'}
-        />
-         <Post
-        nomeUsuario={'Hommer'}
-        fotoUsuario={'https://picsum.photos/50/70'}
-        fotoPost={'https://picsum.photos/200/170'}
-        />
+        {arrayDeComponentes}
       </MainContainer>
     );
   }
 }
+
 
 export default App;
