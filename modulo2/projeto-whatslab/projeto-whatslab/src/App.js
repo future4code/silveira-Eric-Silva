e5b777181c9f23941e0b8ff853b2e01c11bdf8ae
Eric Felipe Silva e Silva <ericfelipess07@gmail.com>
2022-03-27 22:43:24 -0300

    finalizado

--- modulo2/projeto-whatslab/projeto-whatslab/src/App.js
+++ modulo2/projeto-whatslab/projeto-whatslab/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { StyledFooter, Container, StyledMain } from './styles';
+
 
 function App() {
+  const [user,setUser]=useState("");
+  const [message, setMessage]=useState("");
+  const [sendMessage, setSendMessage]=useState([]);
+
+  // console.log(user)  
+  // console.log(message)
+
+
+  const handleClick = () =>{
+    setSendMessage([...sendMessage, {user:user, message:message}])
+  }  
+
+
+  console.log(sendMessage)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container>
+        <StyledMain>
+          {sendMessage.map((message,index) => 
+          <div key={index}>
+            <span>{message.user}: </span>
+            <span>{message.message}</span>
+          </div>
+
+          )} 
+        </StyledMain>
+        <StyledFooter>
+        <div>
+
+          <input onChange={(e)=>setUser(e.target.value) } placeholder='Usuário'/>
+        </div>
+        <div>
+            <input onChange={(e)=> setMessage(e.target.value) } placeholder='Mensagem'/>
+            <button onClick={()=> handleClick(user, message) } >Enviar</button>
+        </div>
+
+        </StyledFooter>
+      </Container>
   );
 }
+
 
 export default App;
