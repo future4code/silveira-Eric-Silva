2fe8fb9a280fd2a34fcfb2d796fc3045c2663d07
Eric Felipe Silva e Silva <ericfelipess07@gmail.com>
2022-03-28 22:25:44 -0300

    Rend-condicional-form

---
+++ modulo2/rend-condicional-form/rend-condicional-form/src/components/Etapas/Etapa1.js
@@ -0,0 +1,17 @@
+const Etapa1 = () => {
+    return(
+        <>
+        <h1>ETAPA 1 - DADOS GERAIS</h1>
+        <p>1.Qual seu nome?</p>
+        <input/>
+        <p>2.Qual sua idade?</p>
+        <input/>
+        <p>3.Qual seu email?</p>
+        <input/>
+        <p>4.Qual sua escolaridade?</p>
+        
+        </>
+    )
+}
+
+export default Etapa1;
---
+++ modulo2/rend-condicional-form/rend-condicional-form/src/components/Etapas/Final.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import { Div } from "../../styles";
+
+export default class Etapa3 extends React.Component {
+    
+    render () {
+  return (
+    <>
+     <h1>O FORMULÁRIO ACABOU</h1>
+     <h2>Muito obrigado por participar! Entraremos em contato!</h2>
+    </>
+  );
+}
+}
