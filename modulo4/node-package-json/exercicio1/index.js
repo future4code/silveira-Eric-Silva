40437700b0945dd61b19d4b918d94c3e2cf02673
Eric Felipe <ericfelipess07@gmail.com>
2022-05-24 16:05:10 -0300

    subindo node-package-json

---
+++ modulo4/node-package-json/exercicio1/index.js
@@ -0,0 +1,17 @@
+// Exercício 1
+
+// a) const a = process.argv[2]
+// ==================================================================
+// b)
+// const name = process.argv[2];
+// const age = Number(process.argv[3]);
+
+// console.log(`Olá, ${name}! Você tem ${age} anos.`);
+// =====================================================================
+// c)
+const name = process.argv[2];
+const age = Number(process.argv[3]);
+
+console.log(`Olá, ${name}! Você terá ${age + 7} anos daqui a 7 anos.`);
+// =====================================================================
+
---
+++ modulo4/node-package-json/exercicio3/index.js
@@ -0,0 +1,16 @@
+// Exercício 3
+
+const tarefas = process.argv[2]
+const listaDeTarefas = ["Lavar a louça", "Cozinhar"]
+ 
+listaDeTarefas.push(tarefas)
+
+console.log("Tarefa Adicionada com sucesso!",listaDeTarefas)
+
+
+
+
+
+
+
+
