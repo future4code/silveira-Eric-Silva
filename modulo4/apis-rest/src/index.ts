80695893052877a9c3feca5ac49d9ce00cb67bc7
Eric Felipe <ericfelipess07@gmail.com>
2022-06-02 14:27:42 -0300

    apis-rest

---
+++ modulo4/apis-rest/src/index.ts
@@ -0,0 +1,84 @@
+import express, {Request, Response} from "express";
+import cors from "cors";
+import { users, User } from "./data";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3003, () => {
+    console.log("Pai ta Online 😎")
+});
+
+app.get("/users", (req:Request, res:Response)=>{
+    res.status(200).send(users)
+})
+
+app.get("/users/:type", (req:Request, res:Response)=>{
+    let errorCode:number = 400
+    try{
+        const type:string = (req.params.type)
+        if (!type){
+            errorCode = 422
+            throw new Error ("Invalid value for type. Please send a string")
+        }
+        const user = users.filter((user)=>{
+            return user.type === type
+        })
+        if(!user){
+            errorCode = 404
+            throw new Error("User a not found.")
+        }
+        res.status(200).send(user)
+    }catch(error){
+        res.status(errorCode).send("Deu ruim carai")
+    }
+    
+ 
+})
+app.get("/users/search/:name", (req:Request, res:Response)=>{
+    let errorCode:number = 400
+    try{
+        const name:string = (req.params.name)
+        if (name!==name){
+            errorCode = 422
+            throw new Error ("Invalid value for name. Please send a string")
+        }
+        const user = users.filter((user)=>{
+            return user.name === name
+        })
+        if(user!==user){
+            errorCode = 404
+            throw new Error("User a not found.")
+        }
+        res.status(200).send(user)
+    }catch(error){
+        res.status(errorCode).send("Deu ruim carai")
+    }
+    
+ 
+})
+
+app.post("/users", (req:Request, res:Response)=>{
+    let errorCode:number = 400
+    try{
+        const {id, name, email, type, age} = req.body
+        if (!id || !name || !email || !type || !age){
+            errorCode = 422
+            throw new Error("Please check the fields!")
+        }
+        const newUser: User ={
+            id,
+            name,
+            email,
+            type,
+            age
+        }
+        users.push(newUser)
+        res.status(201).send({message:"User created successfully"})
+
+    }catch(error){
+        res.status(errorCode).send("Algo de certo está errado")
+    }
+})
