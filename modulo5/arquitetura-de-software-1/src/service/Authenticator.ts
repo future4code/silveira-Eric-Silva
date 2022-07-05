ae12d6cea99be24da35fffeb93b585c7a27ec315
Eric Felipe <ericfelipess07@gmail.com>
2022-07-04 23:08:16 -0300

    arquitetura-de-software-1

---
+++ modulo5/arquitetura-de-software-1/src/service/Authenticator.ts
@@ -0,0 +1,21 @@
+import * as jwt from "jsonwebtoken";
+import { ROLE } from "../model/User";
+
+export interface AuthenticationData {
+  id: string;
+  role: ROLE;
+}
+
+export class Authenticator {
+  public generate(input: AuthenticationData): string {
+    const token = jwt.sign(input, process.env.JWT_KEY as string, {
+      expiresIn: process.env.EXPIRE_TOKEN,
+    });
+    return token;
+  }
+
+  public getTokenData(token: any): AuthenticationData {
+    const data = jwt.verify(token, process.env.JWT_KEY as string);
+    return data as AuthenticationData;
+  }
+}
